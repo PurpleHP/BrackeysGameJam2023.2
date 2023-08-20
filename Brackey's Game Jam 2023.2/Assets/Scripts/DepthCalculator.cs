8d658aa39a3bad172e3608d296c01e0aee3f6ce3
Mert Taylan <ermertay@gmail.com>
2023-08-21 01:39:16 +0300

    depth added
    za

---
+++ Brackey's Game Jam 2023.2/Assets/Scripts/DepthCalculator.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class DepthCalculator : MonoBehaviour
+{
+    [SerializeField] GameObject submarine;
+    private float _depth;
+    private int _intDepth;
+    [SerializeField] float _depthSpeed;
+    [SerializeField] private TextMeshProUGUI _depthText;
+
+    void Update()
+    {
+        _depth = (submarine.transform.position.y * _depthSpeed);
+        _intDepth = (int)Math.Round(_depth);
+        _depthText.text = _intDepth + " m";
+    }
+}
