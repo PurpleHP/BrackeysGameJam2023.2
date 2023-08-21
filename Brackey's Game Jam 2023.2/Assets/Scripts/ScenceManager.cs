ab4601ba99a20b8ff8028efd9988cd44548af247
GreenHP <furkankarakus789@gmail.com>
2023-08-21 21:30:30 +0300

    Main wreeeyyyyyyyyy

--- Brackey's Game Jam 2023.2/Assets/Scripts/ScenceManager.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/ScenceManager.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class SceneManagerScript : MonoBehaviour
 {
+    public Animator animator;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             SceneManager.LoadScene(2);
         }
+    }
+    public void GameScence()
+    {
+        animator.SetBool("IsButtonPressed", true);
+        Debug.Log("Button Pressed");
+        SceneManager.LoadScene(1);
     }
 }
