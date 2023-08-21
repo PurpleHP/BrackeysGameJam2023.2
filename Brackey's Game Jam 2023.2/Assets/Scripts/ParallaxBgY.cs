c2ca249b912a48f10a9d1081511dd58c318a6ef3
Mert Taylan <ermertay@gmail.com>
2023-08-21 14:01:22 +0300

    parallax for game
    +sıkıntıları:
    -sadece y ekseninde tekrarlıyor. x'te  de tekrarlaması için kodla uğraşmak lazım

--- Brackey's Game Jam 2023.2/Assets/Scripts/ParallaxBgY.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/ParallaxBgY.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ParallaxBg : MonoBehaviour
+public class ParallaxBgY : MonoBehaviour
 {
     private float length, startpos;
     public GameObject cam;
@@ -11,18 +11,18 @@ public class ParallaxBg : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
+        startpos = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.y;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float temp = (cam.transform.position.x * (1 - parallaxEffect));
-        float dist = (cam.transform.position.x * parallaxEffect);
+        float temp = (cam.transform.position.y * (1 - parallaxEffect));
+        float dist = (cam.transform.position.y * parallaxEffect);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(transform.position.x, startpos + dist, transform.position.z);
         if (temp > startpos + length) startpos += length;
         else if (temp < startpos - length) startpos -= length;
     }
