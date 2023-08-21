d63968ab1a3a1576609b0e7efe8aeefc97f88a36
GreenHP <furkankarakus789@gmail.com>
2023-08-21 15:56:20 +0300

    Shoping background

---
+++ Brackey's Game Jam 2023.2/Assets/Scripts/ScenceManager.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
