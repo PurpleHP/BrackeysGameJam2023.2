9ded7970322f591ee3aed05f00d73c93c4fa3efc
GreenHP <furkankarakus789@gmail.com>
2023-08-22 15:57:07 +0300

    asgfgagkhasıugfsha

--- Brackey's Game Jam 2023.2/Assets/Scripts/VolumeSettings.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/VolumeSettings.cs
@@ -1,18 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
 public class VolumeSettings : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider SFXSlider;
 }
