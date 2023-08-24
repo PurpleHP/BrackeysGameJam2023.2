b1cdbd44d33c0249082aad525b9d3c50901ae437
Mert Taylan <ermertay@gmail.com>
2023-08-24 17:05:19 +0300

    za

--- Brackey's Game Jam 2023.2/Assets/Scripts/ChangeCinemachineScript.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/ChangeCinemachineScript.cs
@@ -6,9 +6,22 @@ using Cinemachine;
 public class ChangeCinemachineScript : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera cVC;
-
+    [SerializeField] GreenBossRoom GreenBossRoom;
+    [SerializeField] GreenBoss GreenBoss;
+    public GameObject greenBoss;
+    public GameObject submarine;
     private void Update()
     {
-        
+        if (GreenBossRoom.isOnGreenBoss)
+        {
+            cVC.m_Follow = greenBoss.transform;
+            cVC.m_Lens.OrthographicSize = 13.5f;
+        }
+        if(GreenBoss.isDead)
+        {
+            cVC.m_Follow = submarine.transform;
+            cVC.m_Lens.OrthographicSize = 5.6f;
+        }
     }
+    
 }
--- Brackey's Game Jam 2023.2/Assets/Scripts/GreenBossRoom.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/GreenBossRoom.cs
@@ -6,10 +6,12 @@ public class GreenBossRoom : MonoBehaviour
 {
     [SerializeField] PolygonCollider2D exit;
     [SerializeField] PolygonCollider2D enter;
+    public bool isOnGreenBoss = false;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            isOnGreenBoss = true;
             enter.isTrigger = false;
             exit.isTrigger = false;
         }
