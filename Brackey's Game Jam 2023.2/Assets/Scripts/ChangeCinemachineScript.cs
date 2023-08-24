4598287eff4c515057c67d8f0964d879bb05ebc5
Mert Taylan <ermertay@gmail.com>
2023-08-24 16:00:35 +0300

    ğ

--- Brackey's Game Jam 2023.2/Assets/Scripts/ChangeCinemachineScript.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/ChangeCinemachineScript.cs
@@ -1,17 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class ChangeCinemachineScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private CinemachineVirtualCamera cVC;
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
