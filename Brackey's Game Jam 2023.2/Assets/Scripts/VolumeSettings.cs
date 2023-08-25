d588083d8e73a57b6ca25219eab119dc21ea5263
GreenHP <furkankarakus789@gmail.com>
2023-08-25 13:08:31 +0300

    main menu

--- Brackey's Game Jam 2023.2/Assets/Scripts/VolumeSettings.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/VolumeSettings.cs
@@ -2,9 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.Rendering;
 using UnityEngine.UI;
+
 public class VolumeSettings : MonoBehaviour
 {
-    [SerializeField] private AudioMixer myMixer;
-    [SerializeField] private Slider SFXSlider;
+    [SerializeField] private Slider volumeslider = null;
+    [SerializeField] private Text volumeTextUI = null;
+    private void Start()
+    {
+        LoadValues();
+    }
+    public void VolumeSlider(float volume)
+    {
+        volumeTextUI.text = volume.ToString("0.0");
+    }
+    public void SaveVolumeButton()
+    {
+        float volumeValue = volumeslider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+    void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue"); 
+        volumeslider.value = volumeValue;
+        AudioListener.volume = volumeValue;
+    }
 }
