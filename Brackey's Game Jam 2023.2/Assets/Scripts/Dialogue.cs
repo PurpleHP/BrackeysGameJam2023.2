4867f681f756dbefb359e345ca1da2bee86cd773
Mert Taylan <ermertay@gmail.com>
2023-08-25 17:02:19 +0300

    za

--- Brackey's Game Jam 2023.2/Assets/Scripts/Dialogue.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/Dialogue.cs
@@ -11,11 +11,14 @@ public class Dialogue : MonoBehaviour
     [SerializeField] Health submarineHealth;
     [SerializeField] private float textSpeed;
     [SerializeField] GameObject skipButton;
-    [SerializeField] GameObject buttonOne1;
-    [SerializeField] GameObject buttonOne2;
-    [SerializeField] GameObject buttonTwo1;
-    [SerializeField] GameObject buttonTwo2;
-
+    [SerializeField] GameObject button1_1;
+    [SerializeField] GameObject button1_2;
+    [SerializeField] GameObject button2_1;
+    [SerializeField] GameObject button2_2;
+    [SerializeField] GameObject button3_1;
+    [SerializeField] GameObject button3_2;
+    [SerializeField] GameObject button3_3;
+    [SerializeField] GameObject button3_4;
     void Start()
     {
         text.text = string.Empty;
@@ -32,18 +35,17 @@ public class Dialogue : MonoBehaviour
         {
             if (index == 6)
             {
-                buttonOne1.SetActive(true);
-                buttonOne2.SetActive(true);
-                skipButton.SetActive(false);
+                WaitForDialogue2(button1_1, button1_2, skipButton);
             }
             else if (index == 8)
             {
-                buttonTwo1.SetActive(true);
-                buttonTwo2.SetActive(true);
-                skipButton.SetActive(false);
+                WaitForDialogue2(button2_1, button2_2, skipButton);
+            }
+            else if (index == 11)
+            {
+                WaitForDialogue4(button3_1, button3_2, button3_3, button3_4, skipButton);
             }
             NextLine();
-
         }
         else
         {
@@ -57,6 +59,7 @@ public class Dialogue : MonoBehaviour
         index = 0;
         StartCoroutine(TypeLine());
     }
+
     IEnumerator TypeLine()
     {
         foreach(char c in lines[index].ToCharArray())
@@ -65,6 +68,7 @@ public class Dialogue : MonoBehaviour
             yield return new WaitForSeconds(textSpeed);
         }
     }
+
     void NextLine()
     {
         if(index < lines.Length - 1)
@@ -78,43 +82,94 @@ public class Dialogue : MonoBehaviour
             gameObject.SetActive(false);
         }
     }
+    
+    void Answered4(GameObject button1, GameObject button2, GameObject button3, GameObject button4, GameObject skipButton)
+    {
+        skipButton.SetActive(false);
+        button1.SetActive(true);
+        button2.SetActive(true);
+        button3.SetActive(true);
+        button4.SetActive(true);
+    }
+
+    void Answered2(GameObject button1, GameObject button2, GameObject skipButton)
+    {
+        skipButton.SetActive(false);
+        button1.SetActive(true);
+        button2.SetActive(true);
+    }
+
     public void TrueAnswer()
     {
         if (index == 7)
         {
-            buttonOne1.SetActive(false);
-            buttonOne2.SetActive(false);
-            skipButton.SetActive(true);
+            Answered2(button1_1, button1_2, skipButton);
         }
         else if(index == 9)
         {
-            buttonTwo1.SetActive(false);
-            buttonTwo2.SetActive(false);
-            skipButton.SetActive(true);
+            Answered2(button2_1, button2_2, skipButton);
+        }
+        else if (index == 12)
+        {
+            Answered4(button3_1, button3_2, button3_3, button3_4, skipButton);
 
         }
         lines[index+1] = "The answer was.....  CORRECT!";
         SkipDialogue();
         
     }
+    IEnumerator Die()
+    {
+        yield return new WaitForSeconds(4);
+        submarineHealth.SubmarineHealth--;
+
+    }
+    IEnumerator WaitForDialogue2(GameObject button1, GameObject button2,GameObject skipButton)
+    {
+        skipButton.SetActive(false);
+        yield return new WaitForSeconds(3);
+        button1.SetActive(true);
+        button2.SetActive(true);
+
+    }
+    IEnumerator WaitForDialogue4(GameObject button1, GameObject button2, GameObject button3, GameObject button4, GameObject skipButton)
+    {
+        skipButton.SetActive(false);
+        yield return new WaitForSeconds(3);
+        button1.SetActive(true);
+        button2.SetActive(true);
+        button2.SetActive(true);
+        button2.SetActive(true);
+    }
+
     public void FalseAnswer()
     {
         if (index == 7)
         {
-            buttonOne1.SetActive(false);
-            buttonOne2.SetActive(false);
-            skipButton.SetActive(true);
-
+            Answered2(button1_1, button1_2, skipButton);
         }
         else if (index == 9)
         {
-            buttonTwo1.SetActive(false);
-            buttonTwo2.SetActive(false);
-            skipButton.SetActive(true);
+            Answered2(button2_1, button2_2, skipButton);
         }
-        lines[index + 1] = "The answer was.....  NOT CORRECT...";
-        submarineHealth.SubmarineHealth--;
-        SkipDialogue();
+        else if (index == 12)
+        {
+            Answered4(button3_1, button3_2, button3_3, button3_4, skipButton);
+        }
+        if (submarineHealth.SubmarineHealth-1 == 0)
+        {
+            lines[index + 1] = "The answer was.....  NOT CORRECT... You have no HP left so we have to restart. I'm sorry...";
+            SkipDialogue();
+            skipButton.SetActive(false);
+            StartCoroutine(Die());
+        }
+        else
+        {
+            submarineHealth.SubmarineHealth--;
+            lines[index + 1] = "The answer was.....  NOT CORRECT...";
+            SkipDialogue();
+        }
+      
         
     }
 }
