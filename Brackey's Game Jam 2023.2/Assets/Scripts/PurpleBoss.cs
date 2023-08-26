005a78f5ed28869c515072739f591fad961f264d
Mert Taylan <ermertay@gmail.com>
2023-08-27 00:25:05 +0300

    we

--- Brackey's Game Jam 2023.2/Assets/Scripts/PurpleBoss.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/PurpleBoss.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PurpleBoss : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] public float purpleBossHealth;
+
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet"))
+        {
+            purpleBossHealth--;
+        }
     }
 }
