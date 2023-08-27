2e7de893d1ffa19c74b1f5b6a6b8d119152c1ac2
GreenHP <furkankarakus789@gmail.com>
2023-08-27 11:18:18 +0300

    full screen

--- Brackey's Game Jam 2023.2/Assets/Scripts/FullScreen.cs
+++ Brackey's Game Jam 2023.2/Assets/Scripts/FullScreen.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class FullScreen : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F11))
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+            Debug.Log("Screen Changed");
+        }
+    }
     public void Change()
     {
         Screen.fullScreen = !Screen.fullScreen;
